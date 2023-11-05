b949d5498d70d59de465ab30951be2414d0c9f8c
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-11-05 23:50:45 +0100

    Add SCI packages

---
+++ src/sci.ads
@@ -0,0 +1,9 @@
+-- --------------------------------------------------------------------
+--  sci -- scientific collection of tools
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--  SPDX-License-Identifier: Apache-2.0
+-----------------------------------------------------------------------
+
+package SCI is
+
+end SCI;
