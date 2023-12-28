99bce8d52983bf7d11635dc0577ed0b5c3be52a4
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-12-28 23:01:27 +0100

    Moved SCI packages to the ada-sci library

--- src/sci.ads
+++
@@ -1,9 +0,0 @@
--- --------------------------------------------------------------------
---  sci -- scientific collection of tools
---  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
---  SPDX-License-Identifier: Apache-2.0
------------------------------------------------------------------------
-
-package SCI is
-
-end SCI;
